7cdea7cc2ab61a3809ce3ad6d77b609eb025f446
prakash-a-mfai <164123831+prakash-a-mfai@users.noreply.github.com>
2024-05-10 12:56:24 +0530

    .

---
+++ password indicator/script.js
@@ -0,0 +1,12 @@
+var input=document.getElementById("input")
+var res=document.getElementById("res");
+var show=document.getElementsByClassName("indice")
+function display(){
+ 
+    //   if(input.textContent){
+   
+        document.getElementById("weak").style.backgroundColor="red"
+        document.getElementById("weak").style.visibility="visible"
+        // show.style.visibility="visible" 
+         //   }
+}
